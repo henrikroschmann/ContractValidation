37a098f329b4fae43b66f311224e22bdfe5e2b79
Henrik Röschmann <henrik.roschmann@gmail.com>
2024-06-16 14:12:53 +0200

    Moved interface to abstractions

---
+++ src/ContractValidation.Application/Abstractions/IContractsValidationService.cs
@@ -0,0 +1,8 @@
+﻿using ContractValidation.Application.Common;
+
+namespace ContractValidation.Application.Abstractions;
+
+public interface IContractsValidationService
+{
+    ValidationResult<T> Validate<T>(T input) where T : class, new();
+}
--- src/ContractValidation.Application/Services/IContractsValidationService.cs
+++
@@ -1,8 +0,0 @@
-﻿using ContractValidation.Application.Common;
-
-namespace ContractValidation.Application.Services;
-
-public interface IContractsValidationService
-{
-	ValidationResult<T> Validate<T>(T input) where T : class, new();
-}
